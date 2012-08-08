20bed09ba5e5d70d141b9d846a43278d0d7a5efa
Enefsy <spencer@enefsy.com>
2012-08-07 23:55:04 -0700

    Added backwards compatability to android:background in regards to imagebuttons in main layout

--- gen/com/enefsy/main/R.java
+++ gen/com/enefsy/main/R.java
@@ -199,12 +199,10 @@ public final class R {
         public static final int facebook_button=0x7f020001;
         public static final int foursquare_button=0x7f020002;
         public static final int google_button=0x7f020003;
-        public static final int ic_action_search=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int launcher_icon=0x7f020006;
-        public static final int share_via_barcode=0x7f020007;
-        public static final int shopper_icon=0x7f020008;
-        public static final int twitter_button=0x7f020009;
+        public static final int launcher_icon=0x7f020004;
+        public static final int share_via_barcode=0x7f020005;
+        public static final int shopper_icon=0x7f020006;
+        public static final int twitter_button=0x7f020007;
     }
     public static final class id {
         /**  Messages IDs 
